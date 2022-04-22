0ec52f8ee5cd83427ab5f5fd53b79fa4fc3aab66
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-04-22 10:54:51 +0930

    added indexedDB functions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,20 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.log(content);
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const request = await tx.objectStore('jate').put({id: '1', content});
+  console.log('data saved to db');
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const request = tx.objectStore('jate').getAll();
+  const result = await request;
+  return result[0].content;
+};
 
 initdb();
